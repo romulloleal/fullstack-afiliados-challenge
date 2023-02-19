6615fd078a79fd4f4b3183c385ff244a21279ef4
Romullo Leal <romulloleal@gmail.com>
2023-02-19 17:59:53 -0300

    backend::add middleware to ensure user is authenticated, add types definition for express identify user on header authorization

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import ensureAuthenticated from '@middlewares/ensureAuthenticated'
+
 import usersRouter from './users.routes'
 import sessionsRouter from './sessions.routes'
 import transactionsRouter from './transactions.routes'
@@ -8,6 +10,6 @@ const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
-routes.use('/transactions', transactionsRouter)
+routes.use('/transactions', ensureAuthenticated, transactionsRouter)
 
 export default routes
