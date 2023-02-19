89cdd5dfc745e314cc62dee2df1a34dc60d5a0a9
Romullo Leal <romulloleal@gmail.com>
2023-02-19 15:37:10 -0300

    backend::uploadTransactionFile route and multer setup

--- backend/src/routes/transactions.routes.ts
+++ backend/src/routes/transactions.routes.ts
@@ -1,5 +1,24 @@
 import { Router } from 'express'
 
+import multer from '@config/upload'
+import AppError from '@errors/AppError'
+
 const transactionsRouter = Router()
+
+transactionsRouter.post(
+  '/uploadTransactionFile',
+  multer.single,
+  async (request, response) => {
+    const { file } = request
+
+    if (!file) {
+      throw new AppError('Arquivo de transações não informado')
+    }
+
+    return response.json({
+      status: 'success',
+    })
+  }
+)
 
 export default transactionsRouter
