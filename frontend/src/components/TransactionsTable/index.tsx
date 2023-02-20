05e84b8daffa1782158a1429f1078da8b5cb0d0d
Romullo Leal <romulloleal@gmail.com>
2023-02-20 04:32:21 -0300

    frontend::integrated TransactionsTable component with getTransaction, add infinite scroll lib

--- frontend/src/components/TransactionsTable/index.tsx
+++ frontend/src/components/TransactionsTable/index.tsx
@@ -77,52 +77,54 @@ export const TransactionsTable = () => {
                 ))}
               </tbody>
             </DataTable>
-          </InfiniteScroll>
 
-          {/* display when scren has less than 700px */}
-          <DataBox>
-            {transactions.map((transaction) => (
-              <div className='transaction' key={transaction.id}>
-                <div className='row'>
-                  <span className='title'>Produto:</span>
-                  <span className='description'>{transaction.product}</span>
+            {/* display when scren has less than 700px */}
+            <DataBox>
+              {transactions.map((transaction) => (
+                <div className='transaction' key={transaction.id}>
+                  <div className='row'>
+                    <span className='title'>Produto:</span>
+                    <span className='description'>{transaction.product}</span>
+                  </div>
+                  <div className='row'>
+                    <span className='title'>Vendedor:</span>
+                    <span className='description'>
+                      {transaction.sellerName}
+                    </span>
+                  </div>
+                  <div className='row'>
+                    <span className='title'>Tipo:</span>
+                    <span className='description'>
+                      {transaction.transactionType.description}
+                    </span>
+                  </div>
+                  <div className='row'>
+                    <span className='title'>Valor:</span>
+                    <span
+                      className={`description ${
+                        transaction.transactionType.nature === 'Entrada'
+                          ? 'income'
+                          : 'outcome'
+                      }`}
+                    >
+                      {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                      }).format(transaction.price / 100)}
+                    </span>
+                  </div>
+                  <div className='row'>
+                    <span className='title'>Data:</span>
+                    <span className='description'>
+                      {new Intl.DateTimeFormat('pt-BR', {}).format(
+                        new Date(transaction.saleDate)
+                      )}
+                    </span>
+                  </div>
                 </div>
-                <div className='row'>
-                  <span className='title'>Vendedor:</span>
-                  <span className='description'>{transaction.sellerName}</span>
-                </div>
-                <div className='row'>
-                  <span className='title'>Tipo:</span>
-                  <span className='description'>
-                    {transaction.transactionType.description}
-                  </span>
-                </div>
-                <div className='row'>
-                  <span className='title'>Valor:</span>
-                  <span
-                    className={`description ${
-                      transaction.transactionType.nature === 'Entrada'
-                        ? 'income'
-                        : 'outcome'
-                    }`}
-                  >
-                    {new Intl.NumberFormat('pt-BR', {
-                      style: 'currency',
-                      currency: 'BRL',
-                    }).format(transaction.price / 100)}
-                  </span>
-                </div>
-                <div className='row'>
-                  <span className='title'>Data:</span>
-                  <span className='description'>
-                    {new Intl.DateTimeFormat('pt-BR', {}).format(
-                      new Date(transaction.saleDate)
-                    )}
-                  </span>
-                </div>
-              </div>
-            ))}
-          </DataBox>
+              ))}
+            </DataBox>
+          </InfiniteScroll>
         </>
       )}
     </Container>
